9c7410e611382c89380572e85054605c343fa3f3
Kas Boyd <kboyd@meazurelearning.com>
2024-07-25 09:47:36 -0700

    cleaned up syntax for consistency

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import './styles.css'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <div className='container'>
+      <div className="container">
         <Header></Header>
       </div>
-      <footer className='footer'>
-        <p className='footer'>Footer Content here</p>
-      </footer>
+      
+        <Footer></Footer>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import '../styles.css';
 export default function Header() {
     return(
         <div className="header">
-            <img className='logo' src='logo.png' alt="moviedux" />
-            <h2 className='app-subtitle'>It's time for popcorn, find your next movie here!</h2>
+            <img className="logo" src="logo.png" alt="moviedux" />
+            <h2 className="app-subtitle">It's time for popcorn, find your next movie here!</h2>
         </div>
     );
 }
