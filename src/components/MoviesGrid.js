34a293461fb7a677e1de0aa04cbab410de7cc11c
Kas Boyd <kboyd@meazurelearning.com>
2024-07-26 12:22:07 -0700

    added data from json file to movies grid

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -11,6 +11,19 @@ export default function MoviesGrid(){
     },[])
 
     return(
-        <div>{movies.length}</div>
+        <div className="movies-grid">
+            {
+                movies.map(movie => (
+                    <div key={movie.id} className="movie-card">
+                        <img src={`images/${movie.image}`} alt={movie.title}/>
+                        <div className="movie-card-info">
+                            <h3 className="movie-card-title"> {movie.title}</h3>
+                            <p className="movie-card-genre"> {movie.genre}</p>
+                            <p className="movie-card-rating"> {movie.rating}</p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     );
 }
