e656cc2e14ccbecea1b39256429cf93aa562b12e
Kas Boyd <kboyd@meazurelearning.com>
2024-07-26 13:24:31 -0700

    added dynamic searching

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,23 +1,39 @@
-import  React, { useState, useEffect } from "react";
-import '../styles.css'; 
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 import MovieCard from "./MovieCard";
 
-export default function MoviesGrid(){
-    const [movies, setMovies] = useState([]); 
+export default function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    useEffect(() => {
-        fetch("movies.json")
-        .then(response => response.json())
-        .then(data => setMovies(data))
-    },[])
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
 
-    return(
-        <div className="movies-grid">
-            {
-                movies.map(movie => (
-                    <MovieCard movie={ movie } key={movie.id}></MovieCard>
-                ))
-            }
-        </div>
-    );
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) => 
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search movies..."
+        className="search-input"
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id}></MovieCard>
+        ))}
+      </div>
+    </div>
+  );
 }
