b4f6fbc541ba67c33648faa60c5110b39ad17ea4
Kas Boyd <kboyd@meazurelearning.com>
2024-07-26 13:06:33 -0700

    added dynamic CSS classes to movie ratings.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,15 @@ export default function MovieCard({ movie }) {
 
     const handleImageError = (e) => {
         e.target.src = "images/default.jpg"
-    }
+    };
+
+    const getRatinginClass = (rating) => {
+        if (rating >= 8 ) return 'rating-good';
+
+        if (rating >= 5 && rating < 8 ) return 'rating-ok';
+        
+        return "rating-bad"; 
+    };
 
     return(
     <div key={movie.id} className="movie-card">
@@ -15,7 +23,7 @@ export default function MovieCard({ movie }) {
         <div className="movie-card-info">
             <h3 className="movie-card-title"> {movie.title}</h3>
             <p className="movie-card-genre"> {movie.genre}</p>
-            <p className="movie-card-rating"> {movie.rating}</p>
+            <p className={`movie-card-rating ${getRatinginClass(movie.rating)}`}> {movie.rating}</p>
         </div>
     </div>
     );
