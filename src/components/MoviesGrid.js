5d8478c05ed37b071c9e7e6db637caca9d0fef4b
Kas Boyd <kboyd@meazurelearning.com>
2024-07-26 12:15:22 -0700

    added main grid for movies, updated app.

---
+++ src/components/MoviesGrid.js
@@ -0,0 +1,16 @@
+import  React, { useState, useEffect } from "react";
+import '../styles.css'; 
+
+export default function MoviesGrid(){
+    const [movies, setMovies] = useState([]); 
+
+    useEffect(() => {
+        fetch("movies.json")
+        .then(response => response.json())
+        .then(data => setMovies(data))
+    },[])
+
+    return(
+        <div>{movies.length}</div>
+    );
+}
